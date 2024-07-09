2757b949c490aee9f0e245c0fb3504f808c9c7ad
ldd <liuyh_em@163.com>
2024-07-09 14:20:54 +0800

    网易云接口查询

---
+++ src/main/java/com/ktvme/songcopyright/spider/NetEaseMusicSearch.java
@@ -0,0 +1,105 @@
+package com.ktvme.songcopyright.spider;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+
+/**
+ * <p>Description: 调用python接口查询网易云音乐信息</p >
+ * <p>Copyright: Copyright (c)2024</p >
+ * <p>Company: </p >
+ * <P>Created Date: 2024年07月04日</P>
+ *
+ * @author LiuYuHan
+ * @version 1.0
+ */
+
+@Slf4j
+public class NetEaseMusicSearch {
+    private static final String pythonScriptPath = "src/main/java/com/ktvme/songcopyright/spider/pyScript/search.py";
+    private static final String pythonInterpreter = "/Users/liuyuhan/opt/anaconda3/envs/video/bin/python3.8";
+
+    @Nullable
+    public static JsonObject searchCopyrightFrom163(String songTitle){
+        String[] cmd = new String[]{pythonInterpreter, pythonScriptPath, songTitle};
+
+        try {
+            ProcessBuilder pb = new ProcessBuilder(cmd);
+            pb.redirectErrorStream(true);
+            Process process = pb.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            StringBuilder output = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                output.append(line);
+            }
+
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                Gson gson = new Gson();
+                return gson.fromJson(output.toString(), JsonObject.class);
+            } else {
+                System.err.println("Python script failed with exit code: " + exitCode);
+                return null;
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+//    public SongCopyrightDO
+
+
+    public static void main(String[] args) {
+        JsonObject a = searchCopyrightFrom163("吹梦到西洲");
+        // 解析result对象
+        assert a != null;
+        JsonObject result = a.getAsJsonObject("result");
+        JsonArray songs = result.getAsJsonArray("songs");
+
+        // 遍历songs数组并提取所需信息
+        for (JsonElement songElement : songs) {
+            JsonObject song = songElement.getAsJsonObject();
+
+            // 获取歌曲名
+            String songName = song.get("name").getAsString();
+
+            // 获取所有歌手的名字拼接起来
+            JsonArray artists = song.getAsJsonArray("artists");
+            StringBuilder artistNames = new StringBuilder();
+            for (JsonElement artistElement : artists) {
+                JsonObject artist = artistElement.getAsJsonObject();
+                if (artistNames.length() > 0) {
+                    artistNames.append("/");
+                }
+                artistNames.append(artist.get("name").getAsString());
+            }
+
+            // 获取专辑名字
+            JsonObject album = song.getAsJsonObject("album");
+            String albumName = album.get("name").getAsString();
+
+            // 获取专辑的公司
+            String company = album.get("company").getAsString();
+
+
+            long publishTimeMillis = album.get("publishTime").getAsLong();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String publishDateFormatted = sdf.format(new java.util.Date(publishTimeMillis));
+            System.out.println("Publish Time: " + publishDateFormatted);
+
+            System.out.println("Song Name: " + songName + " Artists: " + artistNames.toString() + " Album Name: " + albumName + " Company: " + company);
+        }
+    }
+}
+
---
+++ src/main/java/com/ktvme/songcopyright/spider/pyScript/search.py
@@ -0,0 +1,23 @@
+import browsercookie
+import requests
+import urllib3
+import sys
+
+urllib3.disable_warnings()  # 消除 warning   InsecureRequestWarning
+
+
+def read_chrome_cookie(str):
+    # Chrome提前在页面登录成功，这样才能顺利获取到有效的cookies
+    chrome_cookies = browsercookie.chrome()
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    }
+    # 携带本地cookies请求接口
+    response = requests.get('https://music.163.com/api/search/pc?s='+str+'&type=1',
+                            cookies=chrome_cookies, headers=headers, verify=False)
+    result = response.json()
+    return result
+
+
+if __name__ == '__main__':
+    print(read_chrome_cookie(sys.argv[1]))
